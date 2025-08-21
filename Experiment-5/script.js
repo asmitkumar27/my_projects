6e9cd80cf168e0fc76fe664d69b09be563fb8682
Your Name <your_email@example.com>
2025-08-21 14:35:05 +0530

    Expriment-5

---
+++ Experiment-5/script.js
@@ -0,0 +1,16 @@
+const filter = document.getElementById("categoryFilter");
+const products = document.querySelectorAll(".product");
+
+filter.addEventListener("change", () => {
+  const selectedCategory = filter.value;
+
+  products.forEach(product => {
+    const productCategory = product.getAttribute("data-category");
+
+    if (selectedCategory === "all" || productCategory === selectedCategory) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+});
